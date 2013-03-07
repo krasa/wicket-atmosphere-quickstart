f5bd35303fa1a29cd1b798cd1b9da6a78fc1c530
Vojtech Krasa <vojta.krasa@gmail.com>
2013-03-07 23:09:31 +0100

    second page

--- src/main/java/com/mycompany/SecondPage.java
+++ src/main/java/com/mycompany/SecondPage.java
@@ -16,65 +16,41 @@
  */
 package com.mycompany;
 
-import java.util.Date;
-
-import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.atmosphere.EventBus;
 import org.apache.wicket.atmosphere.Subscribe;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-public class HomePage extends WebPage {
+public class SecondPage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
-	private Component timeLabel;
-	private Component messageLabel;
-	private TextField<String> input;
+	protected final Form<Void> form;
 
-	public HomePage(final PageParameters parameters) {
+	public SecondPage(final PageParameters parameters) {
 		super(parameters);
-		add(new BookmarkablePageLink("SecondPage", SecondPage.class));
-		
-		
-		add(timeLabel = new Label("time", Model.of("start")).setOutputMarkupId(true));
-		add(messageLabel = new Label("message", Model.of("-")).setOutputMarkupId(true));
-
-		Form<Void> form = new Form<Void>("form");
+		form = new Form<Void>("form");
 		add(form);
-		form.add(input = new TextField<String>("input", Model.of("")));
 		form.add(new AjaxSubmitLink("send", form) {
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				EventBus.get().post(input.getModelObject());
+				EventBus.get().post("hkj");
 			}
 
 			@Override
 			protected void onError(AjaxRequestTarget target, Form<?> form) {
 			}
 		});
-
 		setVersioned(false);
 	}
 
 	@Subscribe
-	public void updateTime(AjaxRequestTarget target, Date event) {
-		timeLabel.setDefaultModelObject(event.toString());
-		target.add(timeLabel);
+	public void receiveMessage(AjaxRequestTarget target, String message) {
+		target.add(form);
 	}
 
-	@Subscribe
-	public void receiveMessage(AjaxRequestTarget target, String message) {
-		messageLabel.setDefaultModelObject(message);
-		target.add(messageLabel);
-	}
 }
